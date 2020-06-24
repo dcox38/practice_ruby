70bbd2eb145597d92272e5c23213d697232e7edb
Dan <dcox38@gmailL.com>
2020-06-23 20:44:56 -0500

    init

---
+++ inheritance_example.rb
@@ -0,0 +1,61 @@
+class Car
+    def initialize
+      @speed = 0
+      @direction = 'north'
+    end
+  
+    def brake
+      @speed = 0
+    end
+  
+    def accelerate
+      @speed += 10
+    end
+  
+    def turn(new_direction)
+      @direction = new_direction
+    end
+end
+
+class Car
+    def initialize
+        @speed = 0
+        @direction = 'north'
+      end
+
+      def honk_horn
+        puts "Beeeeeeep!"
+      end
+end
+
+class Bike < Car
+    def initialize
+      @speed = 0
+      @direction = 'north'
+    end
+  
+    def brake
+      @speed = 0
+    end
+  
+    def accelerate
+      @speed += 10
+    end
+  
+    def turn(new_direction)
+      @direction = new_direction
+    end
+  
+    def ring_bell
+      puts "Ring ring!"
+    end
+  end
+
+
+  car1 = Car.new 
+
+  car1.honk_horn
+
+  bike1 = Bike.new
+
+  bike1.ring_bell
